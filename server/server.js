137ebd6353d5b4f7c345f0867d0c68d86b708bf9
Vladimir Tambovtsev <tambovcev99@mail.ru>
2017-10-25 15:52:25 +0300

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -50,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-	console.log("Started on port 3000");
+app.listen(port, () => {
+	console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
